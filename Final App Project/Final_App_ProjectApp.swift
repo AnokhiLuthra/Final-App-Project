61eb3e9407633e302f5d7bc8b77751a42a04aca8
AnokhiLuthra <luthra.anokhi@gmail.com>
2023-07-14 07:39:12 -0700

    Initial Commit

---
+++ Final App Project/Final_App_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_App_ProjectApp.swift
+//  Final App Project
+//
+//  Created by Anokhi Luthra on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_App_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
