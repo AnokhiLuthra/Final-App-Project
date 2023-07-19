0bca5d081470366ac64cb7e65861998b583f603a
AnokhiLuthra <luthra.anokhi@gmail.com>
2023-07-19 08:37:33 -0700

    Sample Password Page

--- Final App Project/Passwords.swift
+++ Final App Project/Passwords.swift
@@ -1,13 +1,13 @@
 //
-//  ContentView.swift
+//  Passwords.swift
 //  Final App Project
 //
-//  Created by Anokhi Luthra on 7/14/23.
+//  Created by Anokhi Luthra on 7/19/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct Passwords: View {
     var body: some View {
         NavigationStack {
             ZStack {
@@ -15,23 +15,27 @@ struct ContentView: View {
                     .ignoresSafeArea()
                 ScrollView {
                     VStack (spacing: 20) {
-                        Text("HOME")
-                            .padding()
-                            .font(.system(size: 60))
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("DarkBackground"))
                         VStack (spacing: 20) {
                             Text("What is cybersecurity?")
                                 .frame(maxWidth: .infinity)
                                 .padding()
                                 .background(Color("TextBackground"))
                                 .cornerRadius(30)
-
                             Text("Cybersecurity is the practice of protecting systems, networks, and research from unauthorized access or criminal use. So much of our personal information is stored on the internet now. And so, it is important to protect your information from cyber attacks. Poor cybersecurity raises many risks, including malware erasing your entire system, an attacker altering your systems, or even stealing your credit card information and making unauthorized purchases.")
                                 .padding()
                                 .background(Color("TextBackground"))
                                 .cornerRadius(30)
+
+                            HStack {
+                                Text("Cybersecurity is the practice of protecting systems, networks, and research from unauthorized access or criminal use. So much of our personal information is stored on the internet now. And so, it is important to protect your information from cyber attacks.")
+                                    .padding()
+                                    .background(Color("TextBackground"))
+                                    .cornerRadius(30)
+                                Text("Cybersecurity is the practice of protecting systems, networks, and research from unauthorized access or criminal use. So much of our personal information is stored on the internet now. And so, it is important to protect your information from cyber attacks.")
+                                    .padding()
+                                    .background(Color("TextBackground"))
+                                    .cornerRadius(30)
+                            }
                         }
                         .font(.system(size: 23))
 
@@ -67,11 +71,12 @@ struct ContentView: View {
                 }
             }
         }
+
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Passwords_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Passwords()
     }
 }
