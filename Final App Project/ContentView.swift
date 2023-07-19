c32aee04c8dab733b021d2cbe9bfa0fa41a26960
AnokhiLuthra <luthra.anokhi@gmail.com>
2023-07-19 08:30:45 -0700

    Home Page

--- Final App Project/ContentView.swift
+++ Final App Project/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color("LightBackground")
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack (spacing: 20) {
+                        Text("HOME")
+                            .padding()
+                            .font(.system(size: 60))
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("DarkBackground"))
+                        VStack (spacing: 20) {
+                            Text("What is cybersecurity?")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color("TextBackground"))
+                                .cornerRadius(30)
+
+                            Text("Cybersecurity is the practice of protecting systems, networks, and research from unauthorized access or criminal use. So much of our personal information is stored on the internet now. And so, it is important to protect your information from cyber attacks. Poor cybersecurity raises many risks, including malware erasing your entire system, an attacker altering your systems, or even stealing your credit card information and making unauthorized purchases.")
+                                .padding()
+                                .background(Color("TextBackground"))
+                                .cornerRadius(30)
+                        }
+                        .font(.system(size: 23))
+
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                            Text("Finished learning? Take a quiz to test your knowledge by clicking here.")
+                        }
+                        .font(.system(size: 20))
+
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color("DarkBackground"))
+                        HStack {
+                            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                Text("Page 1")
+                                    .padding(.vertical)
+                            }
+                            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                Text("Page 2")
+                                    .padding(.vertical)
+                            }
+                            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                Text("Page 3")
+                                    .padding(.vertical)
+                            }
+                            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                Text("Page 4")
+                                    .padding(.vertical)
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color("LightButton"))
+                    } .padding(20)
+
+                }
+            }
         }
-        .padding()
     }
 }
 
