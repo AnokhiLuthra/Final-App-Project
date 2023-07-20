19c6a6641fdafcd79efb7f910ac81ba3ec5f1160
AnokhiLuthra <luthra.anokhi@gmail.com>
2023-07-20 08:42:57 -0700

    Password Checker/Generator adjustments

--- Final App Project/PasswordChecker.swift
+++ Final App Project/PasswordChecker.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct PasswordChecker: View {
+    @State private var SpecialCharacters: Bool = true
+    @State private var UpperCase: Bool = true
+    @State private var LowerCase: Bool = true
+    @State private var Numbers: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("What are the requirements for your password?")
+            VStack {
+                Toggle(isOn: $SpecialCharacters) {
+                    Text("Need Special Characters?")
+                }
+                Toggle(isOn: $UpperCase) {
+                    Text("Need Uppercase Characters?")
+                }
+                Toggle(isOn: $LowerCase) {
+                    Text("Need Lowercase Characters?")
+                }
+                Toggle(isOn: $Numbers) {
+                    Text("Need Numbers?")
+                }
+            }
+            .padding()
+            Text("\(SpecialCharacters)" as String)
+        }
     }
 }
 
--- Final App Project/PasswordGenerator.swift
+++ Final App Project/PasswordGenerator.swift
@@ -9,9 +9,84 @@ import SwiftUI
 
 struct PasswordGenerator: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Password")
     }
 }
+/*struct PasswordGenerator: View {
+    @State private var passwordLength: Double = 12.0
+    @State private var includeUppercase: Bool = true
+    @State private var includeLowercase: Bool = true
+    @State private var includeNumbers: Bool = true
+    @State private var includeSpecialCharacters: Bool = true
+    @State var generatedPassword: String = "Slider"
+
+    var body: some View {
+        VStack {
+            Text("Password Generator")
+              .font(.title)
+              .padding()
+            // Password Length Slider
+            HStack {
+              Text("Password Length: \(passwordLength)")
+              Slider(value: $passwordLength, in: 6...30, step: 1.0)
+                .padding(.horizontal)
+            }
+            .padding()
+            // Character Type Selection
+            Toggle(isOn: $includeUppercase) {
+              Text("Include Uppercase Letters")
+            }
+            .padding()
+            Toggle(isOn: $includeLowercase) {
+              Text("Include Lowercase Letters")
+            }
+            .padding()
+            Toggle(isOn: $includeNumbers) {
+              Text("Include Numbers")
+            }
+            .padding()
+            Toggle(isOn: $includeSpecialCharacters) {
+              Text("Include Special Characters")
+            }
+            .padding()
+            // Generate Password Button
+            Button(action: generatePassword) {
+              Text("Generate Password")
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(8)
+            }
+            .padding()
+            // Display Generated Password
+            Text("Generated Password:")
+            Text(generatedPassword)
+              .padding()
+
+        }
+
+        private func generatePassword() {
+            let characterSets: [String] = [
+              includeUppercase ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" :,
+              includeLowercase ? "abcdefghijklmnopqrstuvwxyz" :,
+              includeNumbers ? "0123456789" :,
+              includeSpecialCharacters ? "!@#$%^&*()-_=+[{]}\\|;:’\",<.>/?" : "
+            ]
+              
+            let availableCharacters = characterSets.joined()
+            guard !availableCharacters.isEmpty else {
+              generatedPassword = "Select at least one character type."
+              return
+            }
+            // 1. Create an array of random characters with the desired password length
+            let passwordCharacters = Array(repeating: (), count: Int(passwordLength))
+              .map { _ in availableCharacters.randomElement() }
+            // 2. Combine the characters into a single string to form the password
+            let password = String(passwordCharacters.compactMap { $0 })
+            generatedPassword = password
+        }
+    } 
+}*/
 
 struct PasswordGenerator_Previews: PreviewProvider {
     static var previews: some View {
